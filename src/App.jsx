a146a3653c8ef0c1f34e0acbcf982d17d24a7441
ozodbekyusufbekov101-source <ozodbekyusufbekov101@gmail.com>
2026-06-04 17:47:58 +0500

    HashRouter qo'shildi

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import Nav from "./Components/Nav/Nav"
 import Home from "./Pages/Home/Home"
 import Video from "./Video/Video"
-import {Route,Routes} from "react-router-dom"
+import { HashRouter, Route, Routes } from "react-router-dom" // HashRouter qo'shildi
+
 function App() {
   return (
-    <>
-    <Nav />
-    <Routes >
-      <Route  path="/" element={<Home />}/>
-      <Route path="/video/:categoryId/:videoId" element={<Video/>}/>
-    </Routes>
-    </>
+    <HashRouter> {/* Barcha routing HashRouter ichida bo'lishi kerak */}
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/video/:categoryId/:videoId" element={<Video />} />
+      </Routes>
+    </HashRouter>
   )
 }
 
